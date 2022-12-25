b9ce3e0c9e0fbb0b2572cfe35c84691eb31b4975
Aleksandar <86465951+aebiz-aleksandar@users.noreply.github.com>
2022-12-25 16:00:56 +0100

    Add files via upload

---
+++ stepSectionData.js
@@ -0,0 +1,54 @@
+export const stepSectionData = {
+  sectionPlans: [
+    {
+      name: 'arcade',
+      price: {
+        monthly: 9,
+        yearly: 90
+      },
+      icon: 'icon-arcade.svg'
+    },
+    {
+      name: 'advanced',
+      price: {
+        monthly: 12,
+        yearly: 120
+      },
+      icon: 'icon-advanced.svg'
+    },
+    {
+      name: 'pro',
+      price: {
+        monthly: 15,
+        yearly: 150
+      },
+      icon: 'icon-pro.svg'
+    }
+  ],
+  sectionAddons: [
+    {
+      name: 'online service',
+      desc: 'Access to multiplayer games',
+      price: {
+        monthly: 1,
+        yearly: 10
+      }
+    },
+    {
+      name: 'larger storage',
+      desc: 'Extra 1TB of cloud save',
+      price: {
+        monthly: 2,
+        yearly: 20
+      }
+    },
+    {
+      name: 'customizable profile',
+      desc: 'Custom theme on your profile',
+      price: {
+        monthly: 2,
+        yearly: 20
+      }
+    }
+  ]
+}
