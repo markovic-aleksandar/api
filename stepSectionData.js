b5221c592a254fb820cead80b7e1c65be1c96b66
Aleksandar <86465951+aebiz-aleksandar@users.noreply.github.com>
2022-12-26 21:18:50 +0100

    Update stepSectionData.js

--- stepSectionData.js
+++ stepSectionData.js
@@ -1,53 +1,53 @@
 {
-  sectionPlans: [
+  "sectionPlans": [
     {
-      name: 'arcade',
-      price: {
-        monthly: 9,
-        yearly: 90
+      "name": "arcade",
+      "price": {
+        "monthly": 9,
+        "yearly": 90
       },
-      icon: 'icon-arcade.svg'
+      "icon": "icon-arcade.svg"
     },
     {
-      name: 'advanced',
-      price: {
-        monthly: 12,
-        yearly: 120
+      "name": "advanced",
+      "price": {
+        "monthly": 12,
+        "yearly": 120
       },
-      icon: 'icon-advanced.svg'
+      "icon": "icon-advanced.svg"
     },
     {
-      name: 'pro',
-      price: {
-        monthly: 15,
-        yearly: 150
+      "name": 'pro',
+      "price": {
+        "monthly": 15,
+        "yearly": 150
       },
-      icon: 'icon-pro.svg'
+      "icon": "icon-pro.svg"
     }
   ],
-  sectionAddons: [
+  "sectionAddons": [
     {
-      name: 'online service',
-      desc: 'Access to multiplayer games',
-      price: {
-        monthly: 1,
-        yearly: 10
+      "name": "online service",
+      "desc": "Access to multiplayer games",
+      "price": {
+        "monthly": 1,
+        "yearly": 10
       }
     },
     {
-      name: 'larger storage',
-      desc: 'Extra 1TB of cloud save',
-      price: {
-        monthly: 2,
-        yearly: 20
+      "name": "larger storage",
+      "desc": "Extra 1TB of cloud save",
+      "price": {
+        "monthly": 2,
+        "yearly": 20
       }
     },
     {
-      name: 'customizable profile',
-      desc: 'Custom theme on your profile',
-      price: {
-        monthly: 2,
-        yearly: 20
+      "name": "customizable profile",
+      "desc": "Custom theme on your profile",
+      "price": {
+        "monthly": 2,
+        "yearly": 20
       }
     }
   ]
